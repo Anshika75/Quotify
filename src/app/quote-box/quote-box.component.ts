e7a32dd31767ceb21d1ed9333e9e83740569ce94
Demi Lune <anshikathakur596@gmail.com>
2024-09-09 15:40:52 +0530

    quote box created

---
+++ src/app/quote-box/quote-box.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quote-box',
+  standalone: true,
+  imports: [],
+  templateUrl: './quote-box.component.html',
+  styleUrl: './quote-box.component.css'
+})
+export class QuoteBoxComponent {
+
+}
