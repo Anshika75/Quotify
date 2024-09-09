8ecde3e3ec4d10c50344d03a9567bfe42b095085
Demi Lune <anshikathakur596@gmail.com>
2024-09-09 15:44:58 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { QuoteBoxComponent } from "./quote-box/quote-box.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, QuoteBoxComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
